b2dcc0d2b06721b5478041ddb05420d5febbb5e8
abdirhmn <134765788+abdirhmn@users.noreply.github.com>
2023-06-04 09:50:42 +0300

    Add files via upload

---
+++ tubedown.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, HTTPException
+from pytube import YouTube
+
+app = FastAPI()
+
+@app.get("/download")
+async def download_video(url: str):
+    try:
+        # Create a YouTube object
+        yt = YouTube(url)
+
+        # Get the highest resolution video stream
+        stream = yt.streams.get_highest_resolution()
+
+        # Download the video to the current working directory
+        stream.download()
+
+        return {"message": "Video downloaded successfully!"}
+    except KeyError:
+        raise HTTPException(status_code=400, detail="Error: Video is not available or cannot be downloaded")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Error: Invalid URL")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Error downloading video: " + str(e))
